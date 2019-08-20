78a7e58eac5e5920e74ec150dc37040955f36ead
Marcelo Cantos <cantosm@anz.com>
2019-08-20 23:11:40 +1000

    Codegen seq.Setable implementation for entity pks

--- sysl2/language/go/pkg/relgom/entity_pk.go
+++ sysl2/language/go/pkg/relgom/entity_pk.go
@@ -4,10 +4,14 @@ import (
 	. "github.com/anz-bank/sysl/sysl2/codegen/golang" //nolint:golint,stylecheck
 )
 
+const pkRecv = "k"
+
 func (g *entityGenerator) goAppendPKDecls(decls []Decl) []Decl {
 	if g.haveKeys {
 		decls = append(decls,
 			g.goPKTypeDecl(),
+			g.goPKSetableHash(),
+			g.goPKSetableEqual(),
 		)
 	}
 	return decls
@@ -22,4 +26,47 @@ func (g *entityGenerator) goPKTypeDecl() Decl {
 
 func (g *entityGenerator) pkFields() []Field {
 	return g.goFieldsForSyslAttrDefs(g.isPkeyAttr, false, nil)
+}
+
+func (g *entityGenerator) pkMethod(f FuncDecl) *FuncDecl {
+	f.Recv = Fields(Field{
+		Names: Idents(pkRecv),
+		Type:  Star(I(g.pkName)),
+	}).Parens()
+	return &f
+}
+
+func (g *entityGenerator) goPKSetableHash() Decl {
+	args := []Expr{I("i")}
+	for _, field := range g.pkFields() {
+		args = append(args, Dot(I(pkRecv), field.Names[0].Name.Text))
+	}
+	return g.pkMethod(FuncDecl{
+		Name: *I("Hash"),
+		Type: FuncType{
+			Params:  *Fields(Field{Names: Idents("i"), Type: I("uint32")}),
+			Results: Fields(Field{Type: I("uint32")}),
+		},
+		Body: Block(
+			Return(Call(g.relgomlib("Hash"), args...)),
+		),
+	})
+}
+
+func (g *entityGenerator) goPKSetableEqual() Decl {
+	return g.pkMethod(FuncDecl{
+		Name: *I("Equal"),
+		Type: FuncType{
+			Params:  *Fields(Field{Names: Idents("i"), Type: Composite(I("interface"))}),
+			Results: Fields(Field{Type: I("bool")}),
+		},
+		Body: Block(
+			If(
+				Init("l", "ok")(Assert(I("i"), Star(I(g.pkName)))),
+				I("ok"),
+				Return(Binary(I(pkRecv), "==", I("l"))),
+			),
+			Return(I("false")),
+		),
+	})
 }
--- sysl2/language/go/pkg/relgom/relgomlib/entity.go
+++ sysl2/language/go/pkg/relgom/relgomlib/entity.go
@@ -1,8 +1,12 @@
 package relgomlib
 
 import (
+	"fmt"
+	"hash/crc32"
+	"math"
 	"strings"
 
+	"github.com/mediocregopher/seq"
 	"github.com/pkg/errors"
 )
 
@@ -44,4 +48,62 @@ func PanicIfRequiredFieldsNotSet(entityMasks []uint64, requiredFieldsMasks []uin
 		}
 		panic(errors.Errorf("required field%s %s not set", quantifier, strings.Join(missingFields, ", ")))
 	}
+}
+
+func Hash(i uint32, values ...interface{}) uint32 {
+	for _, v := range values {
+		i = hash(v, i)
+	}
+	return i
+}
+
+func hash(v interface{}, i uint32) uint32 {
+	switch vt := v.(type) {
+	case seq.Setable:
+		return vt.Hash(i)
+	case uint:
+		return hashUint64(uint64(vt), i)
+	case uint8:
+		return hashUint32(uint32(vt), i)
+	case uint16:
+		return hashUint32(uint32(vt), i)
+	case uint32:
+		return hashUint32(vt, i)
+	case uint64:
+		return hashUint64(vt, i)
+	case int:
+		return hashUint64(uint64(vt), i)
+	case int8:
+		return hashUint32(uint32(vt), i)
+	case int16:
+		return hashUint32(uint32(vt), i)
+	case int32:
+		return hashUint32(uint32(vt), i)
+	case int64:
+		return hashUint64(uint64(vt), i)
+	case float32:
+		return hashUint32(math.Float32bits(vt), i)
+	case float64:
+		return hashUint64(math.Float64bits(vt), i)
+	case string:
+		return hashBytes([]byte(vt), i)
+	case []rune:
+		return hashBytes([]byte(string(vt)), i)
+	case []byte:
+		return hashBytes(vt, i)
+	default:
+		panic(fmt.Sprintf("unhashable: %T", vt))
+	}
+}
+
+func hashUint32(v uint32, i uint32) uint32 {
+	return 1928474805 * (v + i) // Random multiplier
+}
+
+func hashUint64(v uint64, i uint32) uint32 {
+	return uint32(7216620393653828965 * (v + uint64(i))) // Random multiplier
+}
+
+func hashBytes(v []byte, i uint32) uint32 {
+	return 1243383301 * (crc32.ChecksumIEEE(v) + i) // Random multiplier
 }
